a55b864cf7c211e14d8d182310e597b9b5d961af
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-06-28 09:18:24 +1200

    Add introductory text

---
+++ src/Overlap1.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Overlap1 = () => {
+  return (
+    <main>
+      <h1>Overlaps 1</h1>
+      <p>
+        The Jefferson system allows us to transcribe when two (or more) people
+        are speaking at the same time. When transcribing overlaps, we wrap the
+        overlapping talk in square brackets. Here are some examples
+      </p>
+      <div className="examples">[oh no!] [that's great] [I see]</div>
+      <p>
+        The example below is supposed to be an overlap. However, it's missing
+        the square brackets. Add them and press "Submit".
+      </p>
+    </main>
+  )
+}
+
+export default Overlap1
