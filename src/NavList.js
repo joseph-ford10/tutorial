fe6513757a1bd2046fd504104535da195a104947
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-06-28 18:15:39 +1200

    Add more styling

--- src/NavList.js
+++ src/NavList.js
@@ -2,13 +2,16 @@ import React from 'react'
 
 const NavList = () => {
   return (
-    <ul className="navList">
+    <ul>
       <li>
-        <h1>Overlaps</h1>
+        <h1 className="listMainHead">Tutorials</h1>
+        <ul>
+          <h2 className="listSubHead">Overlaps</h2>
+          <ul>Overlaps 1: Square Brackets</ul>
+          <ul>Overlaps 2: Positioning</ul>
+          <ul>Overlaps 3: Connecting Lines</ul>
+        </ul>
       </li>
-      <li>Test2</li>
-      <li>Test3</li>
-      <li>Test4</li>
     </ul>
   )
 }
