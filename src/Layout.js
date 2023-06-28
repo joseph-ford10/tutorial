f6efd8052aec1819a981c3276fb7e99479261374
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-06-28 18:32:13 +1200

    Add header

--- src/Layout.js
+++ src/Layout.js
@@ -8,6 +8,8 @@ import NextPageButton from './NextPageButton'
 import { useEffect } from 'react'
 import NavList from './NavList'
 import ResourceList from './ResourceList'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = ({
   title,
@@ -25,32 +27,36 @@ const Layout = ({
   const [wrongAnswer, setWrongAnswer] = useState(false)
 
   return (
-    <div className="wrapper">
-      <NavList />
-      <main>
-        <h1>{title}</h1>
-        <p>{explanation}</p>
-        <div className="examples">
-          <p>{examples[0]}</p> <p>{examples[1]}</p> <p>{examples[2]}</p>
-        </div>
-        <h2>Exercise</h2>
-        <p>{exerciseIntro}</p>
-        <Exercise
-          text={exerciseText}
-          correctAnswer={correctAnswer}
-          correctAnswerFull={correctAnswerFull}
-          wrongAnswer={wrongAnswer}
-          setWrongAnswer={setWrongAnswer}
-          complete={complete}
-          setComplete={setComplete}
-          setHowComplete={setHowComplete}
-        />
-        {wrongAnswer === true ? <WrongAnswer /> : ''}
-        {complete === true ? <CompletedText howComplete={howComplete} /> : ''}
-        {complete === true ? <NextPageButton nextPage={nextPage} /> : ''}
-      </main>
-      <ResourceList />
-    </div>
+    <>
+      <Navbar />
+      <div className="wrapper">
+        <NavList />
+        <main>
+          <h1>{title}</h1>
+          <p>{explanation}</p>
+          <div className="examples">
+            <p>{examples[0]}</p> <p>{examples[1]}</p> <p>{examples[2]}</p>
+          </div>
+          <h2>Exercise</h2>
+          <p>{exerciseIntro}</p>
+          <Exercise
+            text={exerciseText}
+            correctAnswer={correctAnswer}
+            correctAnswerFull={correctAnswerFull}
+            wrongAnswer={wrongAnswer}
+            setWrongAnswer={setWrongAnswer}
+            complete={complete}
+            setComplete={setComplete}
+            setHowComplete={setHowComplete}
+          />
+          {wrongAnswer === true ? <WrongAnswer /> : ''}
+          {complete === true ? <CompletedText howComplete={howComplete} /> : ''}
+          {complete === true ? <NextPageButton nextPage={nextPage} /> : ''}
+        </main>
+        <ResourceList />
+      </div>
+      {/* <Footer /> */}
+    </>
   )
 }
 
