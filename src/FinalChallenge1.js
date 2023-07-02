477e8b795d8ff46fb5578d0941df2ab0672e38ce
Joseph Ford <122236639+joseph-ford10@users.noreply.github.com>
2023-07-02 12:10:42 +1200

    Add text to header

---
+++ src/FinalChallenge1.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const FinalChallenge1 = () => {
+  return <div>FinalChallenge1</div>
+}
+
+export default FinalChallenge1
--- src/TopNav.js
+++ src/TopNav.js
@@ -4,6 +4,7 @@ const TopNav = () => {
   return (
     <div className="top-nav">
       <h1 className="top-nav-heading">Jefferson Transcription Tutorial</h1>
+      <h2 className="top-nav-subheading">home | about </h2>
     </div>
   )
 }
